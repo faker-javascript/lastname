a1626bb6828d5056fef14eeca82a317027798634
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-18 21:30:23 +0300

    fix(core): fix issue with file path

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+import path from 'node:path';
 import {loadJsonFileSync} from 'load-json-file';
 
 export default function lastName(options) {
     options = options || {};
     const locale = options.locale === undefined ? 'en_US' : options.locale;
-    const lastnames = loadJsonFileSync(`./locales/${locale}/lastnames.json`);
+    const filePath = `./locales/${locale}/lastnames.json`;
+    let lastnames = [];
+
+    try {
+        lastnames = loadJsonFileSync(filePath);
+    } catch {
+        lastnames = loadJsonFileSync(path.resolve('node_modules/@fakerjs/lastname/', filePath));
+    }
+
     return lastnames[Math.floor(Math.random() * lastnames.length)];
 }
