f1798472eb79bf64fd5fc5083ef40e371194d325
Sergey Romanenko <sergey.romanenko@flextype.org>
2022-01-24 23:18:10 +0300

    refactor(core): improve and simplify logic

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import path from 'node:path';
+import fs from 'node:fs';
 import {loadJsonFileSync} from 'load-json-file';
 
 export default function lastName(options) {
     options = options || {};
-    const locale = options.locale === undefined ? 'en_US' : options.locale;
+    const locale = options.locale || 'en_US';
     const filePath = `./locales/${locale}/lastnames.json`;
     let lastnames = [];
-
-    try {
-        lastnames = loadJsonFileSync(filePath);
-    } catch {
-        lastnames = loadJsonFileSync(path.resolve('node_modules/@fakerjs/lastname/', filePath));
-    }
-
+    lastnames = fs.existsSync(path.resolve(filePath)) ? loadJsonFileSync(filePath) : loadJsonFileSync(path.resolve('node_modules/@fakerjs/lastname/', filePath));
     return lastnames[Math.floor(Math.random() * lastnames.length)];
 }
